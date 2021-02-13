dda26a16d1e8afaf33466f431d91a99a7ff360ea
Huzaifa Zia <57328992+huzaifazia17@users.noreply.github.com>
2021-02-12 23:50:11 -0500

    Add files via upload

---
+++ Csharp review- Letter/Csharp review- Letter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// PSUEDOCODE 
+//START
+// Get variable for letter and input
+// Start while loop for if letter is less than a or greater thaz z
+// display good job when lowercase letter entered
+//END
+
+namespace Csharp_review__Letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            char letter = 'A';
+            string input = "";
+
+            //start while loop
+            while (letter < 'a' || letter > 'z')
+            {
+                //ask user to enter letter
+                Console.Write("Please enter a lowercase letter:" );
+                input = Console.ReadLine();
+                //convert to char
+                letter = Convert.ToChar(input[0]);
+            }
+
+            Console.WriteLine("Good Job");
+
+
+            Console.ReadKey();
+        }
+    }
+}
